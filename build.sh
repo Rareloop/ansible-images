6acb92e27f243a2dc127422a39dfcd321c0bf964
Tom Harper <tom.harper@rareloop.com>
2022-05-19 11:54:18 +0100

    variabilise the ansible version in the bash script

--- build.sh
+++ build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+ANSIBLE_VERSION=4.8.0
+
+if [ -n "$1" ]; then
+    ANSIBLE_VERSION=$1;
+    shift 1
+fi
+
 # Ansible 4.8.0
-(docker build --build-arg ANSIBLE_VERSION=4.8.0 . -t tomb1n0/ansible:4.8.0 && docker push tomb1n0/ansible:4.8.0)
+(docker build --build-arg ANSIBLE_VERSION=${ANSIBLE_VERSION} . -t tomb1n0/ansible:${ANSIBLE_VERSION} && docker push tomb1n0/ansible:${ANSIBLE_VERSION})
 
