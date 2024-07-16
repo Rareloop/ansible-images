a3ab7553a3fc3244feba4599d9a945f2e6496751
Tom Harper <tom.harper@rareloop.com>
2024-07-16 13:56:58 +0100

    Update build.sh to be a bit more dumb, just try building all the variations

--- build.sh
+++ build.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+# You can get an idea for which alpine version you need by looking here:
+# https://pkgs.alpinelinux.org/packages?name=python3&branch=v3.15&repo=&arch=&maintainer=
+#
+# You can check what Python3 version is available for which Alpine Linux version and adjust accordingly.
+#
+# You should check https://endoflife.date/ansible to consider which Python version is suitable.
 
-# ANSIBLE_VERSION=4.8.0
-# ALPINE_LINUX_VERSION=3.15
-ANSIBLE_VERSION=2.10.0
-ALPINE_LINUX_VERSION=3.15
-
-if [ -n "$1" ]; then
-    ANSIBLE_VERSION=$1;
-    shift 1
-fi
-
-# Ansible 4.8.0
-# (docker build --build-arg ANSIBLE_VERSION=${ANSIBLE_VERSION} --build-arg ALPINE_LINUX_VERSION=$ALPINE_LINUX_VERSION. -t rareloop/ansible:${ANSIBLE_VERSION} && docker push rareloop/ansible:${ANSIBLE_VERSION})
-docker build --build-arg ANSIBLE_VERSION=${ANSIBLE_VERSION} --build-arg BASE_IMAGE=alpine:${ALPINE_LINUX_VERSION} . -t rareloop-local/ansible:${ANSIBLE_VERSION}
+docker build --build-arg ANSIBLE_VERSION=2.10.0 --build-arg BASE_IMAGE=alpine:3.13 . -t rareloop-local/ansible:2.10.0
+docker build --build-arg ANSIBLE_VERSION=3.4.0 --build-arg BASE_IMAGE=alpine:3.15 . -t rareloop-local/ansible:3.4.0
+docker build --build-arg ANSIBLE_VERSION=4.8.0 --build-arg BASE_IMAGE=alpine:3.15 . -t rareloop-local/ansible:4.8.0
+docker build --build-arg ANSIBLE_VERSION=4.10.0 --build-arg BASE_IMAGE=alpine:3.15 . -t rareloop-local/ansible:4.10.0
+docker build --build-arg ANSIBLE_VERSION=5.5.0 --build-arg BASE_IMAGE=alpine:3.16 . -t rareloop-local/ansible:5.5.0
+docker build --build-arg ANSIBLE_VERSION=5.8.0 --build-arg BASE_IMAGE=alpine:3.16 . -t rareloop-local/ansible:5.8.0
+docker build --build-arg ANSIBLE_VERSION=5.10.0 --build-arg BASE_IMAGE=alpine:3.16 . -t rareloop-local/ansible:5.10.0
+docker build --build-arg ANSIBLE_VERSION=6.7.0 --build-arg BASE_IMAGE=alpine:3.16 . -t rareloop-local/ansible:6.7.0
+docker build --build-arg ANSIBLE_VERSION=7.7.0 --build-arg BASE_IMAGE=alpine:3.18 . -t rareloop-local/ansible:7.7.0
+docker build --build-arg ANSIBLE_VERSION=8.7.0 --build-arg BASE_IMAGE=alpine:3.18 . -t rareloop-local/ansible:8.7.0
+docker build --build-arg ANSIBLE_VERSION=9.7.0 --build-arg BASE_IMAGE=alpine:3.20 . -t rareloop-local/ansible:9.7.0
+docker build --build-arg ANSIBLE_VERSION=10.1.0 --build-arg BASE_IMAGE=alpine:3.20 . -t rareloop-local/ansible:10.1.0
 
